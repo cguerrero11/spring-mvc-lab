28ce0d218cfd4f227f305dea2e0413232363969a
Chris <cguerrero1@my.wctc.edu>
2021-04-21 21:53:14 -0500

    Finished lab

---
+++ src/main/java/edu/wctc/streetfood/MenuItem.java
@@ -0,0 +1,13 @@
+package edu.wctc.streetfood;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MenuItem {
+    private int id;
+    private String name;
+    private String description;
+    private String imageFile;
+}
